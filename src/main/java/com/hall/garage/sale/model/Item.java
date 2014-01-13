2363058b495502f79cfd0cf5c5925113457d7baf
polinchakb <polinchakb@LinuxMint>
2014-01-13 15:00:47 -0500

    Signed-off-by: polinchakb <polinchakb@LinuxMint>

--- src/main/java/com/hall/garage/sale/model/Item.java
+++ src/main/java/com/hall/garage/sale/model/Item.java
@@ -10,7 +10,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
