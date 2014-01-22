75e9d8baba11b2eedd75ba35d7aa1c099de874e6
polinchw <polinchw@netscape.net>
2014-01-22 15:34:37 -0500

    Commit from JBoss Tools

--- src/main/java/com/hall/garage/sale/ds/DataSourceEJB.java
+++ src/main/java/com/hall/garage/sale/ds/DataSourceEJB.java
@@ -5,21 +5,21 @@ import javax.annotation.sql.DataSourceDefinition;
 import javax.ejb.Singleton;
 import javax.sql.DataSource;
 
-@DataSourceDefinition(name = "jdbc:/garagesale",
-minPoolSize = 0,
-initialPoolSize = 0,
-className = "com.mysql.jdbc.Driver",
-serverName="127.8.131.130",
-portNumber=3306,
-user = "adminCEfcLrc",
-password = "WBM9W3C5BJea",
-databaseName = "garagesale",
-properties = {"createDatabase=create"}
-)
-@Singleton
+//@DataSourceDefinition(name = "jdbc:/garagesale",
+//minPoolSize = 0,
+//initialPoolSize = 0,
+//className = "com.mysql.jdbc.Driver",
+//serverName="127.8.131.130",
+//portNumber=3306,
+//user = "adminCEfcLrc",
+//password = "WBM9W3C5BJea",
+//databaseName = "garagesale",
+//properties = {"createDatabase=create"}
+//)
+//@Singleton
 public class DataSourceEJB {
 	
-	@Resource(lookup="jdbc:/garagesale")
-	private DataSource ds;
+//	@Resource(lookup="jdbc:/garagesale")
+//	private DataSource ds;
 
 }
